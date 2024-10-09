e2477b04531a1296a3aab96e93bbffb4ba1e8265
emangbolehsehansitu <136932079+emangbolehsehansitu@users.noreply.github.com>
2024-10-09 23:10:01 +0700

    Update tugaspart1.js

--- quiz/part1/tugaspart1.js
+++ quiz/part1/tugaspart1.js
@@ -44,3 +44,23 @@ function fpb(angka1, angka2) {
   console.log(fpb(22, 99)); // 11
   console.log(fpb(24, 36)); // 12
   console.log(fpb(17, 23)); // 1
+
+function cariMedian(arr) {
+    // you can only write your code here!
+
+    let Cek = Math.floor(arr.length / 2)
+
+    if (arr.length % 2 !== 0) {
+        return Cek;
+    } else {
+        return (arr[Cek - 1] + arr[Cek]) / 2;
+    }
+
+  }
+
+  // TEST CASES
+  console.log(cariMedian([1, 2, 3, 4, 5])); // 3
+  console.log(cariMedian([1, 3, 4, 10, 12, 13])); // 7
+  console.log(cariMedian([3, 4, 7, 6, 10])); // 6
+  console.log(cariMedian([1, 3, 3])); // 3
+  console.log(cariMedian([7, 7, 8, 8])); // 7.5
