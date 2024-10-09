5d81426e7394fd8542aac039f684667aa9d399f1
emangbolehsehansitu <136932079+emangbolehsehansitu@users.noreply.github.com>
2024-10-09 22:55:11 +0700

    Update tugaspart1.js

--- quiz/part1/tugaspart1.js
+++ quiz/part1/tugaspart1.js
@@ -21,3 +21,26 @@ function angkaPrima(angka) {
   console.log(angkaPrima(6)); // false
   console.log(angkaPrima(23)); // true
   console.log(angkaPrima(33)); // false
+
+//cari faktor persekutuan terbesar
+function fpb(angka1, angka2) {
+    // you can only write your code here!
+
+    let iniFpb = ""
+
+    for (let i = 0; i < Math.min(angka1,angka2); i++) {
+        
+        if (angka1 % i == 0 && angka2 % i == 0) {
+            iniFpb = i
+        }
+    }
+
+    return iniFpb;
+  }
+  
+  // TEST CASES
+  console.log(fpb(12, 16)); // 4
+  console.log(fpb(50, 40)); // 10
+  console.log(fpb(22, 99)); // 11
+  console.log(fpb(24, 36)); // 12
+  console.log(fpb(17, 23)); // 1
