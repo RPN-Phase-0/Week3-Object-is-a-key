916173e71f1ad1c38b199cbdcd3cb25f079f1ebb
emangbolehsehansitu <136932079+emangbolehsehansitu@users.noreply.github.com>
2024-10-11 00:32:03 +0700

    Update ujian.js

--- quiz/ujian/ujian.js
+++ quiz/ujian/ujian.js
@@ -129,3 +129,234 @@ console.log(naikAngkot([['Dimitri', 'B', 'F'], ['Icha', 'A', 'B']]));
 //   { penumpang: 'Icha', naikDari: 'A', tujuan: 'B', bayar: 2000 } ]
 
 console.log(naikAngkot([])); //[]
+
+function highestScore (students) {
+  // Code disini
+
+  if (students.length == 0) {
+    return []
+  }
+
+  let hasil = {}
+
+  students.forEach(student => {
+    let nama = student.name;
+    let kelas = student.class;
+    let skor = student.score;
+
+    // Jika kelas belum ada di hasil, tambahkan siswa tersebut
+    if (!hasil[kelas]) {
+      hasil[kelas] = {
+        name: nama,
+        score: skor
+      };
+    } else {
+      // Jika kelas sudah ada, periksa apakah skornya lebih tinggi
+      if (skor > hasil[kelas].score) {
+        hasil[kelas] = {
+          name: nama,
+          score: skor
+        };
+      }
+    }
+  });
+
+  
+
+
+  return hasil
+}
+
+// TEST CASE
+console.log(highestScore([
+  {
+    name: 'Dimitri',
+    score: 90,
+    class: 'foxes'
+  },
+  {
+    name: 'Alexei',
+    score: 85,
+    class: 'wolves'
+  },
+  {
+    name: 'Sergei',
+    score: 74,
+    class: 'foxes'
+  },
+  {
+    name: 'Anastasia',
+    score: 78,
+    class: 'wolves'
+  }
+]));
+
+// {
+//   foxes: { name: 'Dimitri', score: 90 },
+//   wolves: { name: 'Alexei', score: 85 }
+// }
+
+
+console.log(highestScore([
+  {
+    name: 'Alexander',
+    score: 100,
+    class: 'foxes'
+  },
+  {
+    name: 'Alisa',
+    score: 76,
+    class: 'wolves'
+  },
+  {
+    name: 'Vladimir',
+    score: 92,
+    class: 'foxes'
+  },
+  {
+    name: 'Albert',
+    score: 71,
+    class: 'wolves'
+  },
+  {
+    name: 'Viktor',
+    score: 80,
+    class: 'tigers'
+  }
+]));
+
+// // {
+// //   foxes: { name: 'Alexander', score: 100 },
+// //   wolves: { name: 'Alisa', score: 76 },
+// //   tigers: { name: 'Viktor', score: 80 }
+// // }
+
+
+console.log(highestScore([])); //{}
+
+/*
+Implementasikan function graduates untuk mendapatkan daftar student yang lulus dengan aturan:
+
+Student dapat dinyatakan lulus apabila score lebih besar dari 75.
+Masukkan name dan score dari student ke class yang dia ikuti.
+Student yang tidak lulus tidak perlu ditampilkan.
+Output yang diharapkan berupa Object dengan format sebagai berikut:
+
+{
+  <class>: [
+    { name: <name>, score: <score> },
+    ...
+  ],
+  <class>: [
+    { name: <name>, score: <score> },
+    ...
+  ],
+  <class>: [] //NOTE: Jika tidak ada student yang lulus, class ini akan diisi oleh array kosong
+}
+*/
+
+function graduates(students) {
+  // Code disini
+   // Menyimpan hasil dalam objek
+   let result = {};
+
+   // Iterasi melalui setiap siswa
+   students.forEach(student => {
+       let score = student.score;
+       let clas = student.class;
+       let name = student.name;
+
+       // Periksa apakah siswa lulus
+       if (score > 75) {
+           // Jika kelas belum ada, inisialisasi array
+           if (!result[clas]) {
+               result[clas] = []; // Membuat array kosong jika kelas belum ada
+           }
+
+           // Tambahkan siswa yang lulus ke dalam kelas
+           result[clas].push({
+               name: name,
+               score: score
+           });
+       }
+   });
+
+    return result;
+}
+
+console.log(graduates([
+  {
+    name: 'Dimitri',
+    score: 90,
+    class: 'foxes'
+  },
+  {
+    name: 'Alexei',
+    score: 85,
+    class: 'wolves'
+  },
+  {
+    name: 'Sergei',
+    score: 74,
+    class: 'foxes'
+  },
+  {
+    name: 'Anastasia',
+    score: 78,
+    class: 'wolves'
+  }
+]));
+
+// {
+//   foxes: [
+//     { name: 'Dimitri', score: 90 }
+//   ],
+//   wolves: [
+//     { name: 'Alexei' , score: 85 },
+//     { name: 'Anastasia', score: 78 }
+//   ]
+// }
+
+console.log(graduates([
+  {
+    name: 'Alexander',
+    score: 100,
+    class: 'foxes'
+  },
+  {
+    name: 'Alisa',
+    score: 76,
+    class: 'wolves'
+  },
+  {
+    name: 'Vladimir',
+    score: 92,
+    class: 'foxes'
+  },
+  {
+    name: 'Albert',
+    score: 71,
+    class: 'wolves'
+  },
+  {
+    name: 'Viktor',
+    score: 80,
+    class: 'tigers'
+  }
+]));
+
+// {
+//   foxes: [
+//     { name: 'Alexander', score: 100 },
+//     { name: 'Vladimir', score: 92 }
+//   ],
+//   wolves: [
+//     { name: 'Alisa', score: 76 },
+//   ],
+//   tigers: [
+//     { name: 'Viktor', score: 80 }
+//   ]
+// }
+
+
+console.log(graduates([])); //{}
