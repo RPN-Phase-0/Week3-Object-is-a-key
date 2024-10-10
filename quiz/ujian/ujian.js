bd24bd81b0c28f5b9a11b29fec2c7cc18a862e2e
emangbolehsehansitu <136932079+emangbolehsehansitu@users.noreply.github.com>
2024-10-10 16:01:26 +0700

    Create ujian.js

---
+++ quiz/ujian/ujian.js
@@ -0,0 +1,71 @@
+/*
+Implementasikan function deepSum untuk mendapatkan jumlah pertambahan dari angka-angka yang terdapat di dalam array
+*/
+
+function deepSum (arr) {
+  // Code disini
+  
+   // Jika array kosong, kembalikan pesan
+   if (arr.length === 0) {
+    return 'No number';
+  }
+
+  // Variabel untuk menampung jumlah total
+  let total = 0;
+
+  // Looping level pertama (array terluar)
+  for (let i = 0; i < arr.length; i++) {
+    // Looping level kedua
+    for (let j = 0; j < arr[i].length; j++) {
+      // Looping level ketiga (array terdalam)
+      for (let k = 0; k < arr[i][j].length; k++) {
+        // Tambahkan setiap elemen ke total
+        total = total + arr[i][j][k];
+      }
+    }
+  }
+
+  return total;
+}
+
+//TEST CASE
+console.log(deepSum([
+  [
+    [4, 5, 6],
+    [9, 1, 2, 10],
+    [9, 4, 3]
+  ],
+  [
+    [4, 14, 31],
+    [9, 10, 18, 12, 20],
+    [1, 4, 90]
+  ],
+  [
+    [2, 5, 10],
+    [3, 4, 5],
+    [2, 4, 5, 10]
+  ]
+])); // 316
+
+console.log(deepSum([
+  [
+    [20, 10],
+    [15],
+    [1, 1]
+  ],
+  [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+    [2],
+    [9, 11]
+  ],
+  [
+    [3, 5, 1],
+    [1, 5, 3],
+    [1]
+  ],
+  [
+    [2]
+  ]
+])); // 156
+
+console.log(deepSum([])); // No number
