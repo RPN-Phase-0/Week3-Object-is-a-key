7fd1f9ba60ae3c1b2a3f581f7e7666ea1131abed
ihsan <75242608+ihsanoo@users.noreply.github.com>
2024-10-29 00:08:52 +0800

    Push quiz part 2

---
+++ quiz/part2/soal3.js
@@ -0,0 +1,23 @@
+//TIPS: gunakan method toUpperCase() dan toLowerCase()
+function tukarBesarKecil(kalimat) {
+    // you can only write your code here
+    let result = ''
+    for(let i = 0 ; i  < kalimat.length; i++) {
+        if (kalimat[i] >= 'a' && kalimat[i] <= 'z') {
+            result += kalimat[i].toUpperCase();
+        }else if(kalimat[i] >= 'A' && kalimat[i] <= 'Z') {
+            result += kalimat[i].toLowerCase();
+    } else {
+        result += kalimat[i]
+    }
+  }
+  return result;
+}
+  
+  // TEST CASES
+  console.log(tukarBesarKecil('Hello World')); // "hELLO wORLD"
+  console.log(tukarBesarKecil('I aM aLAY')); // "i Am Alay"
+  console.log(tukarBesarKecil('My Name is Bond!!')); // "mY nAME IS bOND!!"
+  console.log(tukarBesarKecil('IT sHOULD bE me')); // "it Should Be ME"
+  console.log(tukarBesarKecil('001-A-3-5TrdYW')); // "001-a-3-5tRDyw"
+
