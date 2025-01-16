b2cbe0c46c50316813d38c3e3e5291084c9693eb
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2025-01-16 19:41:50 +0700

    part2

---
+++ quiz/part2/soal_3.js
@@ -0,0 +1,23 @@
+//TIPS: gunakan method toUpperCase() dan toLowerCase()
+function tukarBesarKecil(kalimat) {
+  let result = '';
+
+  for (let i = 0; i < kalimat.length; i++) {
+    let char = kalimat[i];
+
+    if (char === char.toUpperCase()) {
+      result += char.toLowerCase();
+    } else {
+      result += char.toUpperCase();
+    }
+  }
+
+  return result;
+}
+
+// TEST CASES
+console.log(tukarBesarKecil('Hello World')); // "hELLO wORLD"
+console.log(tukarBesarKecil('I aM aLAY')); // "i Am Alay"
+console.log(tukarBesarKecil('My Name is Bond!!')); // "mY nAME IS bOND!!"
+console.log(tukarBesarKecil('IT sHOULD bE me')); // "it Should Be ME"
+console.log(tukarBesarKecil('001-A-3-5TrdYW')); // "001-a-3-5tRDyw"
